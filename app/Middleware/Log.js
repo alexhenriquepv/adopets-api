8142fd4ba46993fa08eec9239b56683820c7e819
Gitea <gitea@fake.local>
2020-07-04 13:20:22 -0300

    resolve bug for jwt token on login request

--- app/Middleware/Log.js
+++ app/Middleware/Log.js
@@ -4,9 +4,14 @@ const Logger = use('Logger')
 
 class Log {
   async handle ({ auth, request }, next) {
-    const user = await auth.getUser()
-    Logger.transport('file').info(`${user.name || 'anonymous'} - ${request.method()} ${request.url()}`)
-    await next()
+    try {
+      const user = await auth.getUser()
+      Logger.transport('file').info(`${user.name || 'anonymous'} - ${request.method()} ${request.url()}`)
+    }
+    catch (err) {}
+    finally {
+      await next()
+    }
   }
 }
 
